f4e45a2180465cbd41b534dc924d90b401eac96b
kwl027 <126184003+kwl027@users.noreply.github.com>
2023-02-25 11:22:40 +0700

    Create chatbot_pizza.r

---
+++ R_Projects/chatbot_pizza.r
@@ -0,0 +1,5 @@
+#ordering pizza at Pizza Bar
+#pizza
+pizza_id <- c(1,2,3,4)
+pizza_type <- c("Four seasons", "Double cheeze", "Meat feast","BBQ Chiken")
+df_menu <- data.frame(pizza_id, pizza_type)
